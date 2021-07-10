bdf95534cb15a4eccc2caa32c98115c0098e70e5
[Sona] <[sona16061999@gmail.com]>
2021-07-10 14:52:28 +0530

    [Sona] LinkedList is created

---
+++ LinkedList/LinkedListNode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class LinkedListNode<T>
+    {
+        private Node<T> head;
+        //Add the elements in the front
+        public void AddFront( T value )
+        {
+            Node<T> newNode = new Node<T>(value);
+            //check Whether head is null
+            if(head==null)
+            {
+                 head=newNode;
+            }
+            //make newNode as head
+            else
+            {
+                newNode.Next = head;
+                head = newNode;
+            }
+        }
+
+        public void Display()
+        {
+            Node<T> temp = head;
+            if(head==null)
+            {
+                Console.WriteLine("List is empty!!");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                   Console.WriteLine("" + temp.value);
+                   temp = temp.Next;
+                }
+            }
+        }
+            
+    }
+}
